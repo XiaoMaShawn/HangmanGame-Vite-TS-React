e4629e6e01a85a333c534ead3b132a49d71e45d6
XiaoMaShawn <ma2014xiao@hotmail.com>
2023-01-04 14:45:29 -0500

    component structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from "react"
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 import words from './wordList.json'
+import './App.css'
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   console.log(wordToGuess);
 
   return (
-    <div className="container">Hi
+    <div className="container">
+      <div className="message">
+        win/lose
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
 
     </div>
   )
---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,3 @@
+export function HangmanDrawing() {
+  return <div>hangmandrawing</div>
+}
