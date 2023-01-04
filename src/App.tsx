7331fd4d5db3e61bea52b0619e59f2677efef6e1
XiaoMaShawn <ma2014xiao@hotmail.com>
2023-01-04 14:33:35 -0500

    add wordlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-
+import { useState } from "react"
+import words from './wordList.json'
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  })
+
+  console.log(wordToGuess);
+
   return (
     <div className="container">Hi
 
